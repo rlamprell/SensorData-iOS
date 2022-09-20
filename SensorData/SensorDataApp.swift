04dcc31c028e83b61a0571ea089ccbbc72bae5a4
rlamprell <rob.lamprell@hotmail.co.uk>
2022-09-20 10:09:17 +0100

    Initial Commit

---
+++ SensorData/SensorDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensorDataApp.swift
+//  SensorData
+//
+//  Created by Rob Lamprell on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SensorDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
