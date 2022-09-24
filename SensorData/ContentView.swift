cc48199e7486b28d39a77eb9ca1000f32e8f97e0
rlamprell <rob.lamprell@hotmail.co.uk>
2022-09-24 13:41:55 +0100

    conditional appearance of data

--- SensorData/ContentView.swift
+++ SensorData/ContentView.swift
@@ -12,55 +12,29 @@ struct ContentView: View {
     
     @ObservedObject var motion: MotionManager
     @State private var turnOn = false
-//    @State private var showGreeting = true
-    // You need to instanciate your MotionManager class as an ObservedObject to use x, y and z when the update
+
     
     var body: some View {
-        
-        
-        //        HStack {
-        //            Button("Start") {
-        //                motion.start()
-        //            } .padding()
-        //
-        //            Button("Stop") {
-        //                motion.stop()
-        //            } .padding()
-        //        }
         HStack {
             Toggle(motion.getSensorName(), isOn: $turnOn)
-                .onChange(of: turnOn) { value in
+              .onChange(of: turnOn) { value in
+                if turnOn{
                     motion.start()
+                }
+                else {
+                    motion.stop()
+                }
+            }
+
+        }
+        
+        if turnOn{
+            HStack {
+                Text("X: \(motion.x)")
+                Text("Y: \(motion.y)")
+                Text("Z: \(motion.z)")
             }
         }
-        
-        HStack {
-            Text("X: \(motion.x)")
-            Text("Y: \(motion.y)")
-            Text("Z: \(motion.z)")
-        }
-        
-        //        Toggle("", isOn: $isDisplayed)
-        //            .onChange(of: isDisplayed) { value in
-        //                // action...
-        //                print(value)
-        //            }
-        //        VStack {
-        //            Toggle("Show welcome message", isOn: $showGreeting)
-        //
-        //            if showGreeting {
-        //                Text("Hello World!")
-        //            }
-        //        }
-        //    }
-        
-        //        VStack {
-        //            Text(motion.getSensorName())
-        //            Text("X: \(motion.x)")
-        //            Text("Y: \(motion.y)")
-        //            Text("Z: \(motion.z)")
-        //        }
-        //    }
     }
 }
 
